c7ff357649884876bf5905e0350fee238eecff99
EvanMisshula <EMisshula@jjay.cuny.edu>
2011-06-30 01:09:39 -0400

    User model with passwords, and user show page

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ gem 'rails', '3.0.9'
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 gem 'sqlite3', '1.3.3'
-
+gem 'gravatar_image_tag','1.0.0.pre2'
 # Use unicorn as the web server
 # gem 'unicorn'
 
@@ -28,11 +28,12 @@ gem 'sqlite3', '1.3.3'
 # and rake tasks are available in development mode:
 group :development do
   gem 'rspec-rails', '2.6.1'
-#  gem 'annotate-models', '>=1.0.4'
+#  gem 'annotate-models', '1.0.4'
 end
 
 group :test do
   gem 'rspec-rails', '2.6.1'
   gem 'spork', '0.9.0.rc8'
   gem 'webrat', '0.7.1'
+  gem 'factory_girl_rails','1.0'
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,19 +1,48 @@
 require 'spec_helper'
 
-describe UsersController do
+describe UsersController do  #1
   render_views
 
-  describe "GET 'new'" do
-    it "should be successful" do
+  describe "GET 'new'" do  #2
+    it "should be successful" do #3
       get 'new'
       response.should be_success
-    end
+    end #3-
   
-
-
-    it "should have the right title" do
+    it "should have the right title" do #4
       get 'new'
       response.should have_selector("title", :content => "Sign up")
-  end
- end
-end
+  end #4-
+ end #2-
+
+  describe "GET 'show'" do #5
+    
+    before(:each) do  #6
+      @user=Factory(:user)
+    end #6-
+    
+    it "should be successful" do #7
+      get :show, :id => @user
+      response.should be_success
+    end #7-
+
+    it "should find the right user" do #8
+      get :show, :id => @user
+      assigns(:user).should == @user
+    end #8-
+   
+    it "should have the right title" do #9
+      get :show, :id => @user
+      response.should have_selector("title",:content =>@user.name)
+      end #9-
+    it "should include the user's name" do #10
+      get :show, :id => @user
+      response.should have_selector("h1",:content =>@user.name)
+      end #10-
+
+    it "should have a profile image" do #11
+      get :show, :id => @user
+      response.should have_selector("h1>img",:class => "gravatar")
+  end #11-
+  end #5-
+ end #1
